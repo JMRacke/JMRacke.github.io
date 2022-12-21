204f304bdd503d8d9b0de0bc82670ec9ed9a2046
aguilkal <116393918+aguilkal@users.noreply.github.com>
2022-12-21 09:48:08 -0800

    Added more logic to error handling add some const

--- JS/cards-and-maps/restaurant.js
+++ JS/cards-and-maps/restaurant.js
@@ -1,6 +1,7 @@
 import { handleError } from "../error-handling/error.js";
 import { drawMap } from "./drawMap.js";
 import { getData } from "../api-helper/get-data.js";
+import { NO_FOUND_RESTAURANTS } from "../global-constants/constants.js";
 const locationCoords = {
   lat: 0,
   lng: 0,
@@ -10,26 +11,44 @@ export function getRestaurants({ coords: { latitude: lat, longitude: lon } }) {
   locationCoords.lng = Number(lon);
 
   const apiUrl = `https://sde-final-backend.herokuapp.com/api?term=${restaurant_name.value}&latitude=${lat}&longitude=${lon}&radius=40000&open_now=true&sort_by=best_match&limit=20`;
+  debugger;
   getData(apiUrl)
-  .then(({businesses}) => {
-    showRes({businesses});
+  .then((json_payload) => {
+    const restaurants = json_payload.businesses;
+    const apiError = json_payload.error;
+    if (apiError) {
+      handleError();
+    }
+  else if (!restaurants || restaurants.length === 0) {
+    handleError(NO_FOUND_RESTAURANTS)
+  }else {
+    showRes(restaurants);
+  }
   })
 }
 
 export function noTrackGetRestaurants(searchlocation, searchTerm) {
   const apiUrl = `https://sde-final-backend.herokuapp.com/api?location=${searchlocation}&term=${searchTerm}&open_now=true&sort_by=best_match&limit=10`;
-getData(apiUrl)
-.then(({businesses}) => {
-  showRes({businesses});
-})
- 
+  getData(apiUrl)
+  .then((json_payload) => {
+    const restaurants = json_payload.businesses;
+    const apiError = json_payload.error;
+    if (apiError) {
+      handleError();
+    }
+  else if (!restaurants || restaurants.length === 0) {
+    handleError(NO_FOUND_RESTAURANTS)
+  }else {
+    showRes(restaurants);
+  }
+  })
 }
 
-function showRes({ businesses }) {
+function showRes( restaurants ) {
   restaurant_results.innerHTML = "";
   map_container.innerHTML = "";
 
-  for (const rest of businesses) {
+  for (const rest of restaurants) {
     const cardContainer = document.createElement("div");
     cardContainer.className = "card grid-item";
     cardContainer.setAttribute(
---
+++ JS/global-constants/constants.js
@@ -0,0 +1,6 @@
+export const DEFAULT_ERROR_MESSAGE =
+"API call error: Double check api url and make sure the server is up & running.";
+
+export const NO_FOUND_RESTAURANTS = "No restaurants found on the YELP api";
+
+export const NO_TRACKING_MESSAGE = "Please Enter Your Location"
